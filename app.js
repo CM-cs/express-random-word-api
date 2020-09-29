cebf4629709dd957040ad362f68b7edadd5d2efb
Chloe McEntire <chloemcentire@gmail.com>
2020-09-28 23:09:54 -0500

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let cors = require('cors')
 let app = express()
-//const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.use(cors());
 
 let HTTPresponse = ["abstraction", "agile", "algorithm", "API", "application", "array", "artifact", "assertion", "automata", "bandwidth", "benchmark", "binary", "bioinformatics", "bit", "blacklist", "bitmap", "boolean", "booting", "CPU", "cipher", "client", "closure", "cloud", "collection", "compiler", "computation", "cybersecurity", "concatenation", "concurrency", "conditional", "container", "CSV", "daemon", "database", "debugging", "declaration", "DNS", "domain", "encryption", "ethics", "event", "executable", "exception", "expression", "filter", "gigabyte", "handler", "hardware", "hash", "heap", "heapsort", "identifier", "IDE", "inheritance", "integer", "interface", "interpreter", "invariant", "iteration", "kernel", "library", "linker", "linter", "list", "loader", "logic", "matrix", "memory", "mergesort", "method", "modem", "node", "object", "parameter", "peripheral", "pointer", "postcondition", "precondition", "procedure", "Prolog", "Python", "queue", "quicksort", "radix", "record", "recursion", "reference", "regex", "regression", "router", "scalability", "script", "semantics", "sequence", "serialization", "serializability", "set", "software", "stack", "state", "statement", "storage", "stream", "string", "stylesheet", "subroutine", "syntax", "tree", "user", "variable", "waterfall", "spiderbot"];
@@ -37,6 +37,6 @@ app.post("/RandomWord.json", function(req, res) {
 });
 
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("Server started...")
 });
